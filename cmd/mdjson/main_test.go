e60736274c51c41ebbadc74ba630785ae54c35b4
Tobias Rehbein <tobias.rehbein@web.de>
2017-04-28 07:17:52 +0200

    Add missing tests for mdjson dump functionality

--- cmd/mdjson/main_test.go
+++ cmd/mdjson/main_test.go
@@ -8,32 +8,44 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"strings"
 	"testing"
 )
 
-func sampleDataHandler(w http.ResponseWriter, r *http.Request) {
-	f, err := os.Open("../../testdata/sample.html")
-	if err != nil {
-		panic(err)
+func dataHandler(dr io.Reader) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		_, err := io.Copy(w, dr)
+		if err != nil {
+			panic(err)
+		}
 	}
+}
 
-	_, err = io.Copy(w, f)
-	if err != nil {
-		panic(err)
+func codeHandler(c int) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(c)
+		w.Write([]byte(http.StatusText(c)))
 	}
 }
 
 func TestDumpValidData(t *testing.T) {
-	s := httptest.NewServer(http.HandlerFunc(sampleDataHandler))
+	f, err := os.Open("../../testdata/sample.html")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f.Close()
+
+	s := httptest.NewServer(dataHandler(f))
 	defer s.Close()
 
 	var b bytes.Buffer
-	err := dump(s.URL, &b)
+	err = dump(s.URL, &b)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -46,5 +58,43 @@ func TestDumpValidData(t *testing.T) {
 
 	if bytes.Compare(is, expected) != 0 {
 		t.Errorf("dump wrote unexpected data; expected: %q; is: %q", expected, is)
+	}
+}
+
+func TestDumpInvalidData(t *testing.T) {
+	f, err := os.Open("../../testdata/fail.html")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f.Close()
+
+	s := httptest.NewServer(dataHandler(f))
+	defer s.Close()
+
+	var b bytes.Buffer
+	err = dump(s.URL, &b)
+	if err == nil {
+		t.Error("expected error did not occur")
+	}
+	if err != nil && !strings.HasPrefix(err.Error(), "Unable to parse running order structure ") {
+		t.Errorf("Unexpected err; expected: \"Unable to parse running order structure...\"; is: %v", err)
+	}
+}
+
+func TestDumpRemoteError(t *testing.T) {
+	c := 404
+	s := httptest.NewServer(codeHandler(c))
+	defer s.Close()
+
+	var b bytes.Buffer
+	err := dump(s.URL, &b)
+	if err == nil {
+		t.Error("expected error did not occur")
+	}
+
+	st := http.StatusText(c)
+	expectedSuffix := fmt.Sprintf(" returned \"%d %s\"", c, st)
+	if err != nil && !strings.HasSuffix(err.Error(), expectedSuffix) {
+		t.Errorf("Unexpected err; expected: '...returned %q'; is: %v", st, err)
 	}
 }
